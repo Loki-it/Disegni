cd9f037de1507797183aa9bcb335419b64237593
Loki-it <60159705+Loki-it@users.noreply.github.com>
2020-06-26 20:08:13 +0200

    Add files via upload

---
+++ disegno-custom-2n-2c.py
@@ -0,0 +1,20 @@
+# Specificare i 2 numeri prima di farlo partire
+n1 = int(input('numero1: '))
+n2 = int(input('numero2: '))
+
+# Specificare i 2 colori prima di farlo partire
+c1 = input('colore1: ')
+c2 = input('colore2: ')
+
+from turtle import *
+title('Disegno Custom')
+color(c1, c2)
+
+begin_fill()
+while True:
+    forward(n1)
+    left(n2)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
